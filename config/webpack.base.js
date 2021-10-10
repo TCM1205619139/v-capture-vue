e4ee9d7482ea7b0418f4db182edf2e13bbb05fd6
TCM1205619139 <1205619139@qq.com>
2021-10-10 18:29:02 +0800

    chg: url-loader转base64大小限制

--- config/webpack.base.js
+++ config/webpack.base.js
@@ -84,7 +84,7 @@ module.exports = {
         test: /\.(mp4|webm|ogg|mp3|wav|flac|aac)(\?.*)?$/,
         loader: 'url-loader',
         options: {
-          limit: 10000,
+          limit: 1024,
           name: 'media/[name].[hash:7].[ext]'
         }
       },
@@ -92,7 +92,7 @@ module.exports = {
         test: /\.(woff2?|eot|ttf|otf)(\?.*)?$/,
         loader: 'url-loader',
         options: {
-          limit: 10000,
+          limit: 1024,
           name: 'fonts/[name].[hash:7].[ext]'
         }
       }
