b5ac25223221aa74e69f0cdce2ec99d21f12bc70
TCM1205619139 <1205619139@qq.com>
2021-10-20 21:57:26 +0800

    feat: 添加message通信方法字典

--- src/util/message.ts
+++ src/util/message.ts
@@ -1,5 +1,58 @@
 import { ExtensionPageType, Notify } from './notify'
 
+const MessageDict = [
+  {
+    local: ExtensionPageType.Content,
+    origin: ExtensionPageType.Popup,
+    send: chrome.runtime.sendMessage,
+  },
+  {
+    local: ExtensionPageType.Content,
+    origin: ExtensionPageType.Background,
+    send: chrome.runtime?.sendMessage
+  },
+  {
+    local: ExtensionPageType.Content,
+    origin: ExtensionPageType.Inject,
+    send: window.postMessage
+  },
+  {
+    local: ExtensionPageType.Inject,
+    origin: ExtensionPageType.Popup,
+    send: chrome.runtime?.sendMessage
+  },
+  {
+    local: ExtensionPageType.Inject,
+    origin: ExtensionPageType.Background,
+    send: chrome.runtime?.sendMessage
+  },
+  {
+    local: ExtensionPageType.Inject,
+    origin: ExtensionPageType.Content,
+    send: window.postMessage
+  },
+  {
+    local: ExtensionPageType.Popup,
+    origin: ExtensionPageType.Content,
+    send: chrome.tabs?.sendMessage
+  },
+  {
+    local: ExtensionPageType.Popup,
+    origin: ExtensionPageType.Inject,
+    send: chrome.tabs?.sendMessage
+  },
+  {
+    local: ExtensionPageType.Background,
+    origin: ExtensionPageType.Content,
+    send: chrome.tabs?.sendMessage
+  },
+  {
+    local: ExtensionPageType.Background,
+    origin: ExtensionPageType.Inject,
+    send: chrome.tabs?.sendMessage
+  }
+]
+
 /**
  * chrome插件各模块的短链接消息通知
  */
