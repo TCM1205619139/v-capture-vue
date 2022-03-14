04b453b88c05137ac3d3df8df48044f8589e12d4
TCM1205619139 <1205619139@qq.com>
2022-03-14 19:16:47 +0800

    fix: quote fix

--- babel.config.ts
+++ babel.config.ts
@@ -5,20 +5,20 @@ module.exports = {
       {
         useBuiltIns: 'usage',
         corejs: 3
-      },
+      }
     ],
     [
       '@babel/preset-typescript',
       {
         allExtensions: true, // 支持所有文件扩展名，否则在vue文件中使用ts会报错
-      },
-    ],
+      }
+    ]
   ],
   plugins: [
     [
       '@babel/plugin-transform-runtime',
       {
-        corejs: 3,
+        corejs: 3
       }
     ]
   ],
